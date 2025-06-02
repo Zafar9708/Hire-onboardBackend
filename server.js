ecef5c2790c51d521e21c27abcfb9094b5360be6
Zafar9708 <aseem@kloudrac.com>
2025-06-02 16:28:06 +0530

    port changes

--- server.js
+++ server.js
@@ -106,18 +106,15 @@ app.get('/auth/google/callback', async (req, res) => {
   });
 
 
-mongoose
-    .connect(process.env.MONGO_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    .then(() => {
-        console.log('✅ MongoDB connected');
-        const PORT = process.env.PORT || 5000;
-        app.listen(PORT, () => {
-            console.log(`🚀 Server running on http://localhost:${PORT}`);
-        });
-    })
-    .catch(err => {
-        console.error('❌ MongoDB connection error:', err.message);
-    });
+  mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+      console.log('✅ MongoDB connected');
+      const PORT = process.env.PORT || 5000;
+      app.listen(PORT, () => {
+          console.log(`🚀 Server running on port ${PORT}`);
+      });
+  })
+  .catch(err => {
+      console.error('❌ MongoDB connection error:', err.message);
+  });
